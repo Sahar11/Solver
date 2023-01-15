923d09b9ba1cac36aa12c8219117e4ba940226b4
Sahar11 <saharhalim151@gmail.com>
2023-01-15 17:30:49 -0500

    add client and server

---
+++ client/script.js
@@ -0,0 +1,121 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+// loading dots when AI is getting information
+function loader(element) {
+  // Update the text content of the loading indicator
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+      // If the loading indicator has reached three dots, reset it
+    element.textContent += '.';
+    if(element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300)
+}
+
+// for typing the text one by one from the AI
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if(index < text.length) {
+      element.innerHTML += text.charAt(index); // get character under a specific index in the text that AI is going to return and increment the index else simply clear the interval now we can type text and load AI's answers.
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20)
+}
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+// determine whether it's you or the AI
+function chatStripe(isAi, value, uniqueId) {
+  return (
+    `
+    <div class="wrapper ${isAi && 'ai'}">
+    <div class="chat">
+    <div class="profile">
+    <img src="${isAi ? bot : user} "
+    alt="${isAi ? 'bot' : 'user'}"
+    />
+    </div>
+    <div class="message" id=${uniqueId}> ${value}></div>
+    </div>
+    </div>
+    `
+  )
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+
+  //user's chatStripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+ // to clear the textarea input
+  form.reset();
+
+  //bot's chatStripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+  // to focus scroll to the bottom
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  
+  //put specific message in div
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+
+  // fetch data from server -> bot's response
+
+  const response = await fetch('http://localhost:5001', {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if(response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+    //console.log(data)
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = 'Something went wrong';
+
+    alert(err)
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13) {
+    handleSubmit(e);
+  }
+})
